eb597f5710305eaa1ea2ca95226bf87565b83739
Denis Doroshenko <ddtlt1984@gmail.com>
2021-07-02 16:34:25 +0400

    fixed DataHelper methods

--- src/test/java/data/DataHelper.java
+++ src/test/java/data/DataHelper.java
@@ -122,7 +122,7 @@ public class DataHelper {
                 "past, future or nothing for choice a current date (if dateMethod = getDate)",
                 "zero, doubleZero or nothing for choice an incorrect value (from 13 to 99) (if dateMethod != getDate)",
                 "zero, doubleZero or nothing for choice a doubleZero value (if dateMethod != getDate)",
-                "tripleZero, short or for choice a random value",
+                "tripleZero, short or nothing for choice a random value",
                 "badName or nothing for choice a random correct name"
         ) + " <----");
     }
