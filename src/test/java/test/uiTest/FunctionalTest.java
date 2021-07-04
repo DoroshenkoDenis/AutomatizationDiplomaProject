50ad80cf405c961d81fa5d18caa85a38038481b4
Denis Doroshenko <ddtlt1984@gmail.com>
2021-07-04 18:46:31 +0400

    added tests

--- src/test/java/test/uiTest/FunctionalTest.java
+++ src/test/java/test/uiTest/FunctionalTest.java
@@ -1,4 +1,81 @@
 package test.uiTest;
 
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import page.SalesPage;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class FunctionalTest {
+    SalesPage salesPage = new SalesPage();
+
+    @BeforeAll
+    static void setUp() {
+        open("http://localhost:8080");
+    }
+
+    @Test
+    void debitBuyingByApprovedCard() {
+        salesPage.buyByDebit().buyWithCardInfo(
+                "APPROVED",
+                "en",
+                "getDate",
+                "future",
+                "no",
+                "no",
+                "ok",
+                "ok"
+        );
+        salesPage.getSuccessNotification().shouldBe(Condition.visible, Duration.ofMillis(15000));
+    }
+
+    //  Тест должен упасть - Баг - неверное уведомление
+//    @Test
+//    void debitBuyingByDeclinedCard() {
+//        salesPage.buyByDebit().buyWithCardInfo(
+//                "DECLINED",
+//                "en",
+//                "getDate",
+//                "future",
+//                "no",
+//                "no",
+//                "ok",
+//                "ok"
+//        );
+//        salesPage.getErrorNotification().shouldBe(Condition.visible, Duration.ofMillis(15000));
+//    }
+
+    @Test
+    void creditBuyingByApprovedCard() {
+        salesPage.buyByCredit().buyWithCardInfo(
+                "APPROVED",
+                "en",
+                "getDate",
+                "future",
+                "no",
+                "no",
+                "ok",
+                "ok"
+        );
+        salesPage.getSuccessNotification().shouldBe(Condition.visible, Duration.ofMillis(15000));
+    }
+
+    @Test
+    void creditBuyingByDeclinedCard() {
+        salesPage.buyByCredit().buyWithCardInfo(
+                "DECLINED",
+                "en",
+                "getDate",
+                "future",
+                "no",
+                "no",
+                "ok",
+                "ok"
+        );
+        salesPage.getSuccessNotification().shouldBe(Condition.visible, Duration.ofMillis(15000));
+    }
+
 }
