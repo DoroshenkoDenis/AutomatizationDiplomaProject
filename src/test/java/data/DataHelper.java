5a79c9bcc2e3dd0f0a83704b3fa2372dd3350799
Denis Doroshenko <ddtlt1984@gmail.com>
2021-07-07 17:15:12 +0400

    fixed

--- src/test/java/data/DataHelper.java
+++ src/test/java/data/DataHelper.java
@@ -113,14 +113,14 @@ public class DataHelper {
         );
     }
 
-    //             Statuses:
-    //            "APPROVED, DECLINED, short, one or nothing for a random choice",
-    //            "en",
-    //            "getDate or nothing for a incorrect date",
-    //            "past, future or nothing for choice a current date (if dateMethod = getDate)",
-    //            "zero, doubleZero or nothing for choice an incorrect value (from 13 to 99) (if dateMethod != getDate)",
-    //            "zero or nothing for choice a doubleZero value (if dateMethod != getDate)",
-    //            "badName or nothing for choice a random correct name",
-    //            "tripleZero, short or nothing for choice a random value"
+ //             Statuses:
+ //            "APPROVED, DECLINED, short, one or nothing for a random choice",
+ //            "en",
+ //            "getDate or nothing for a incorrect date",
+ //            "past, future or nothing for choice a current date (if dateMethod = getDate)",
+ //            "zero, doubleZero or nothing for choice an incorrect value (from 13 to 99) (if dateMethod != getDate)",
+ //            "zero or nothing for choice a doubleZero value (if dateMethod != getDate)",
+ //            "badName or nothing for choice a random correct name",
+ //            "tripleZero, short or nothing for choice a random value"
 
 }
