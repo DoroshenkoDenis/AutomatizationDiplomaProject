f684e08f7250f1449b59284477d15fb7ccdb90c0
Denis Doroshenko <ddtlt1984@gmail.com>
2021-07-07 17:05:06 +0400

    added API tests

--- src/test/java/test/apiTest/APITest.java
+++ src/test/java/test/apiTest/APITest.java
@@ -1,4 +1,119 @@
 package test.apiTest;
 
+import data.APIHelper;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class APITest {
+
+    @Test
+    void debitBuyingByApprovedCard() {
+        val response = APIHelper.debitBuying(
+                "APPROVED",
+                "en",
+                "getDate",
+                "future",
+                "no",
+                "no",
+                "ok",
+                "ok"
+        )
+                .then()
+                .statusCode(200)
+                .extract().asString();
+        assertTrue(response.contains("APPROVED"));
+    }
+
+    @Test
+    void debitBuyingByDeclinedCard() {
+        val response = APIHelper.debitBuying(
+                "DECLINED",
+                "en",
+                "getDate",
+                "future",
+                "no",
+                "no",
+                "ok",
+                "ok"
+        )
+                .then()
+                .statusCode(200)
+                .extract().asString();
+        assertTrue(response.contains("DECLINED"));
+    }
+
+    @Test
+    void creditBuyingByApprovedCard() {
+        val response = APIHelper.creditBuying(
+                "APPROVED",
+                "en",
+                "getDate",
+                "future",
+                "no",
+                "no",
+                "ok",
+                "ok"
+        )
+                .then()
+                .statusCode(200)
+                .extract().asString();
+        assertTrue(response.contains("APPROVED"));
+    }
+
+    @Test
+    void creditBuyingByDeclinedCard() {
+        val response = APIHelper.creditBuying(
+                "DECLINED",
+                "en",
+                "getDate",
+                "future",
+                "no",
+                "no",
+                "ok",
+                "ok"
+        )
+                .then()
+                .statusCode(200)
+                .extract().asString();
+        assertTrue(response.contains("DECLINED"));
+    }
+
+    @Test
+    void debitBuyingByAnotherCard() {
+        val response = APIHelper.debitBuying(
+                "random",
+                "en",
+                "getDate",
+                "future",
+                "no",
+                "no",
+                "ok",
+                "ok"
+        )
+                .then()
+                .statusCode(500)
+                .extract().asString();
+        assertTrue(response.contains("Bad Request"));
+    }
+
+    @Test
+    void creditBuyingByAnotherCard() {
+        val response = APIHelper.creditBuying(
+                "random",
+                "en",
+                "getDate",
+                "future",
+                "no",
+                "no",
+                "ok",
+                "ok"
+        )
+                .then()
+                .statusCode(500)
+                .extract().asString();
+        assertTrue(response.contains("Bad Request"));
+    }
+
 }
