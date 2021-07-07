7c89c2276ad1a2b86e79c851f8cdff432a1550fb
Denis Doroshenko <ddtlt1984@gmail.com>
2021-07-07 19:59:41 +0400

    expanded DB Helper

--- src/test/java/data/DataBaseHelper.java
+++ src/test/java/data/DataBaseHelper.java
@@ -6,6 +6,7 @@ import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.time.LocalDateTime;
 
 public class DataBaseHelper {
     private final static QueryRunner runner = new QueryRunner();
@@ -17,13 +18,53 @@ public class DataBaseHelper {
     }
 
     @SneakyThrows
-    public static String getDebitBuyingStatus() {
+    public static int getAmountFromPaymentEntity() {
+        return runner.query(conn, "SELECT amount FROM payment_entity", new ScalarHandler<>());
+    }
+
+    @SneakyThrows
+    public static LocalDateTime getCreatedDateFromPaymentEntity() {
+        return runner.query(conn, "SELECT created FROM payment_entity", new ScalarHandler<>());
+    }
+
+    @SneakyThrows
+    public static String getStatusFromPaymentEntity() {
         return runner.query(conn, "SELECT status FROM payment_entity", new ScalarHandler<>());
     }
 
     @SneakyThrows
-    public static String getCreditBuyingStatus() {
+    public static String getTransactionIdFromPaymentEntity() {
+        return runner.query(conn, "SELECT transaction_id FROM payment_entity", new ScalarHandler<>());
+    }
+
+    @SneakyThrows
+    public static String getBankIdFromCreditRequestEntity() {
+        return runner.query(conn, "SELECT bank_id FROM credit_request_entity", new ScalarHandler<>());
+    }
+
+    @SneakyThrows
+    public static LocalDateTime getCreatedDateFromCreditRequestEntity() {
+        return runner.query(conn, "SELECT created FROM credit_request_entity", new ScalarHandler<>());
+    }
+
+    @SneakyThrows
+    public static String getStatusFromCreditRequestEntity() {
         return runner.query(conn, "SELECT status FROM credit_request_entity", new ScalarHandler<>());
+    }
+
+    @SneakyThrows
+    public static LocalDateTime getCreatedDateFromOrderEntity() {
+        return runner.query(conn, "SELECT created FROM order_entity", new ScalarHandler<>());
+    }
+
+    @SneakyThrows
+    public static String getCreditIdFromOrderEntity() {
+        return runner.query(conn, "SELECT credit_id FROM order_entity", new ScalarHandler<>());
+    }
+
+    @SneakyThrows
+    public static String getPaymentIdFromOrderEntity() {
+        return runner.query(conn, "SELECT payment_id FROM order_entity", new ScalarHandler<>());
     }
 
     @SneakyThrows
